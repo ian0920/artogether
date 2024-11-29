e7981d6c9d34910328eb015b49abfd103b79ebcf
Eason Cheng <martin79425@gmail.com>
2024-11-29 09:55:53 +0800

    ApplyPrdReturnDaoImpl添加@Repository

--- src/main/java/com/artogether/product/prd_return/ApplyPrdReturnDao.java
+++ src/main/java/com/artogether/product/prd_return/ApplyPrdReturnDao.java
@@ -4,15 +4,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ApplyPrdReturnDao {
-	
+
 	int add(PrdReturn prdReturn);
-	int update(PrdReturn prdReturn);//軟刪除
+
+	int update(PrdReturn prdReturn);// 軟刪除
+
 	int delete(Integer id);
+
 	PrdReturn findByPK(Integer id); // 查詢單筆申請
+
 	List<PrdReturn> findByStatus(Integer status); // 查詢按狀態篩選的記錄
+
 	List<PrdReturn> findByApplyDateRange(LocalDateTime startDate, LocalDateTime endDate);
+
 	List<PrdReturn> findByType(Integer type); // 0=換貨，1=退貨
+
 	List<PrdReturn> findAllOrderByReturnDateDesc();
 
-
 }
+
--- src/main/java/com/artogether/product/prd_return/ApplyPrdReturnDaoImpl.java
+++ src/main/java/com/artogether/product/prd_return/ApplyPrdReturnDaoImpl.java
@@ -7,7 +7,8 @@ import javax.persistence.EntityManagerFactory;
 
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public class ApplyPrdReturnDaoImpl implements ApplyPrdReturnDao {
 	
 	@Autowired
