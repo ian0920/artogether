540d1b9a831d728a00af514c50556dd0b4353243
Allen.Lee <celovemb@gmail.com>
2024-12-06 23:30:29 +0800

    優化VnePrice，補上Optional邏輯，確認往返資訊

--- src/main/java/com/artogether/venue/vnedto/VnePriceDTO.java
+++ src/main/java/com/artogether/venue/vnedto/VnePriceDTO.java
@@ -15,13 +15,14 @@ public class VnePriceDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer vneId;
-    private String vneName;
+//    private String vneName;
     private Integer defaultPrice;
     // 特定時段的價格
     private Integer price;
     // 開始時間與結束時間
-    private List<Integer> priceTslot;
-    // 星期設定 (例如：'0111110' 表示哪些日子適用此價格)
+    private Integer startTime;
+    private Integer endTime;
+    // 星期設定
     private List<Integer> dayOfWeek;
 
     private LocalDateTime effectiveTime;
