39477b1c692824c1f533595e01944824a0c5c8cd
Allen.Lee <celovemb@gmail.com>
2024-12-06 10:00:31 +0800

    TslotService-getBinaryWeek簡化

--- src/main/java/com/artogether/venue/vneorder/VneOrder.java
+++ src/main/java/com/artogether/venue/vneorder/VneOrder.java
@@ -19,7 +19,6 @@ import java.time.LocalDateTime;
 @Builder
 @Table(name = "vne_order")
 public class VneOrder implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
     @Id
