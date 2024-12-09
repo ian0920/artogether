8fcaf61ea5bc23360c9dea995f7ec673d3940022
Allen.Lee <celovemb@gmail.com>
2024-12-10 00:27:51 +0800

    Tslot前端頁面設計完成

--- src/main/java/com/artogether/venue/vnedto/TslotDTO.java
+++ src/main/java/com/artogether/venue/vnedto/TslotDTO.java
@@ -1,6 +1,7 @@
 package com.artogether.venue.vnedto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TslotDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
