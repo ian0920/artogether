ed9e15145341b3eba0abcf625f07c52163e98cb9
Ian Hsu <ian555077@gmail.com>
2024-12-13 12:06:29 +0800

    商家功能連結綁定屬性 特定頁籤選定

---
+++ src/main/java/com/artogether/util/ScheduleManager.java
@@ -0,0 +1,27 @@
+package com.artogether.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduleManager {
+
+
+    /*
+        活動狀態變更
+             商家 -> 報名中or延期 ->活動結束
+             會員 -> 訂單活動狀態變更
+             會員 -> 取消報名按鈕變更
+             會員 -> 取消報名功能變更
+     */
+
+    /*
+        優惠券狀態變更
+            商家 - end date < 現在日期 -> 狀態更改
+            會員 - 收到日期 + 效期 > 現在日期 -> 狀態更改
+
+
+     */
+
+
+
+}
